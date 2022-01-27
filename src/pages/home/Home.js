274736ff271eb2958ef3c02b7994e259535a334f
alumonabenaiah@gmail.com <alumonabenaiah71@gmail.com>
2022-01-27 05:17:56 +0100

    added device support alert

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Default from "../../comp/Templates/Default";
 import Sidebar from "../../comp/Sidebar/Sidebar";
-import { HiMenuAlt1 } from "react-icons/hi";
-
+import { HiMenuAlt1, HiExclamation } from "react-icons/hi";
+import logo from "../../logo.png";
 import "./style.css";
 
 import DataContext from "../../context/DataContext";
@@ -10,7 +10,28 @@ import DataContext from "../../context/DataContext";
 const ref = React.createRef();
 
 function Home() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+  const [width, setWidth] = useState(0);
   const { downloadAsPdf } = useContext(DataContext);
+
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   function handlePdfDownload() {
     let template = document.querySelector("[data-template-view]");
@@ -35,6 +56,7 @@ function Home() {
             <HiMenuAlt1 className="icon" />
           </div>
         </div>
+        {windowDimensions.width < 768 ? <DeviceSupport /> : ""}
         <div className="templates-container" ref={ref}>
           <Default tref={"ref"} />
         </div>
@@ -44,3 +66,27 @@ function Home() {
 }
 
 export default Home;
+
+function DeviceSupport() {
+  return (
+    <>
+      <div className="device-main">
+        <img src={logo} alt="" />
+        <br />
+        <h1>
+          Sorry. <HiExclamation className="icon" />
+        </h1>
+        <p>Mobile Device not yet supported.</p>
+        <br />
+        <button
+          className="btn home"
+          onClick={() => {
+            window.open("https://github.com/benrobo/quickr");
+          }}
+        >
+          Visit Quickr
+        </button>
+      </div>
+    </>
+  );
+}
