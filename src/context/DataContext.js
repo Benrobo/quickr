795b2ccf0fb91072cfad5cf10e35b4c33f05411b
alumonabenaiah@gmail.com <alumonabenaiah71@gmail.com>
2022-01-27 03:51:08 +0100

    fix bugs

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
-import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
+import { savePDF } from "@progress/kendo-react-pdf";
 
 const DataContext = createContext();
 
